e8335f35ec93fd337c7d0395f831cde75e0c3da6
gaurav-zaiswal <gauravzaiswal@gmail.com>
2020-05-01 20:44:42 +0545

    Updates

---
+++ precache-manifest.7290b0b442ae0d9df2e9c5641249b5b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd541b511023eb586ba6398579af9479",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a6824963631f1e0c066",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "6dc603e36b12ba4b2a0b",
+    "url": "/static/css/main.8b3a6889.chunk.css"
+  },
+  {
+    "revision": "6a6824963631f1e0c066",
+    "url": "/static/js/2.5a8f104e.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.5a8f104e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dc603e36b12ba4b2a0b",
+    "url": "/static/js/main.99b264c1.chunk.js"
+  },
+  {
+    "revision": "fab8a60221159ed68a4b",
+    "url": "/static/js/runtime-main.3d39db7b.js"
+  }
+]);
